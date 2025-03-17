e7d7201ad6be98cfcc0296255087935e16a95de5
riddhi-webds <100923091+riddhi-webds@users.noreply.github.com>
2025-03-17 11:17:59 +0000

    variables nd datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 12345
+let accountEmail = 'riddhi@gmail.com'
+var accountPassword = '1234'
+accountCity = 'jaipur'
+let accountState
+
+// accountId = 2
+
+accountEmail = 'rid@gmail.com'
+accountPassword = '222'
+accountCity = 'Delhi'
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,13 @@
+// number = 2 to power 53
+// BigInt
+// string = ""
+// boolean = true/ false
+// null = standalone value
+// undefined =
+// symbol = Unique
+
+// object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); // object
+
