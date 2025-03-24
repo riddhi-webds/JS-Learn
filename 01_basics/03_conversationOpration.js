b530e735ad829646126a5dadd536562522d1f5e5
riddhi-webds <100923091+riddhi-webds@users.noreply.github.com>
2025-03-24 06:11:37 +0000

    oprations

--- 01_basics/03_conversationOpration.js
+++ 01_basics/03_conversationOpration.js
@@ -1,30 +1,55 @@
 let score = "Riddhi"
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log( valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log( valueInNumber);
 
 
 // "33" = 33
 // "33abc" = NaN
 // true = 1; false = 0
 
- let isLoggedIn = "Riddhi"
+let isLoggedIn = "Riddhi"
 
- let booleanIsLoggedIn = Boolean(isLoggedIn)
- console.log(booleanIsLoggedIn);
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
 
 //  1 = true; 0 = false;
 // "" = false;
 // "Riddhi" =  true
-  
+
 
 let someNumber = 33
 
-let stringNumber = String (someNumber)
-console.log(typeof stringNumber);
+// let stringNumber = String (someNumber)
+// console.log(typeof stringNumber);
 
- 
+// *********************** Oprations ***************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);    
+// console.log(2-2);    
+// console.log(2*2);    
+// console.log(2**3);    
+// console.log(2/3);    
+// console.log(2%3);    
+
+let str1 = "Hello"
+let str2 = " Riddhi"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+let gameCounter = 100
+// ++gameCounter;
+gameCounter++;
+console.log(gameCounter);
+
+
+
