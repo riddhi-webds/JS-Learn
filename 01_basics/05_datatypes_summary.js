39840120df1ad3043c02330cb56ab112a681746a
riddhi-webds <100923091+riddhi-webds@users.noreply.github.com>
2025-03-24 10:49:48 +0000

    datatypes_summary

---
+++ 01_basics/05_datatypes_summary.js
@@ -0,0 +1,36 @@
+// Primitive
+
+// 7 types : string, Number, Boolean, null, Undefined, Symbol, BigInt
+
+const score = 100
+const scoreVale = 100.3
+
+const isLoggIn = false
+const outSideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 234567435n
+
+
+// Reference (NonPrimitive)
+
+// Array, Objects, Functions
+
+const heros = ['shkatiman', 'naagraj', 'doga']
+let myObj = {
+    name: "Riddhi",
+    age: 22,
+};
+
+console.log(typeof anotherId);
+console.log(typeof heros);
+console.log(typeof myObj);
+console.log(typeof outSideTemp);
+
+
+
